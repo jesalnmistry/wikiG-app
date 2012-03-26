2c972ec0211720d6344fa24497613731bc5c7482
jesal mistry <jesalnmistry@gmail.com>
2012-03-26 19:44:54 +0530

    populateDB

---
+++ populateDB.py
@@ -0,0 +1,34 @@
+import nltk, sys, re, os, urllib2, time, threading, Queue, pymongo, operator
+from nltk import *
+from nltk import metrics, stem, tokenize
+from nltk.corpus import PlaintextCorpusReader, stopwords, swadesh
+from twisted.internet import defer, reactor
+from twisted.web.client import getPage
+from BeautifulSoup import BeautifulSoup
+from pymongo import Connection
+from pymongo.errors import ConnectionFailure
+from testImplemented import testImplemented
+
+myfunctions = testImplemented()
+allfilenames = myfunctions.readFilesNameList("/home/jesal/onedump/")
+filenames = []
+for item in allfilenames:
+    filenames.append(item.replace("/home/jesal/onedump/",""))
+    
+
+articlenames = []
+for item in filenames:
+    articlenames.append(item.replace(".txt",""))
+    
+
+wordlist = []
+keywordCountDict = dict()
+for item in articlenames:
+    wordlist = myfunctions.readTextFromDump(item)
+    wordlist = myfunctions.removeEmpty(wordlist)
+    wordlist = myfunctions.removeStopwords(wordlist)
+    keywordCountDict = myfunctions.makedict_keyword(wordlist)
+    sortedkeywordCountDict = myfunctions.decrSortDictOnValue(keywordCountDict)
+    myfunctions.insKeywordCountinDB(sortedkeywordCountDict, 'tempdb', item.lower())
+    
+
